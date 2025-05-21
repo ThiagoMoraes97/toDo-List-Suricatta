d665d2b394d4a195c84c59b4393861283f951eea
Thiago Moraes <thiagomottamoraes@gmail.com>
2025-05-21 19:27:41 -0300

    Update CORS policy and .env files.

--- todo-backend/src/server.js
+++ todo-backend/src/server.js
@@ -9,8 +9,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
+const allowedOrigins = [
+  "http://localhost:3000",      
+  "http://127.0.0.1:3000",       
+  "http://3.148.108.139:3000",   
+];
+
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost"]
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 
